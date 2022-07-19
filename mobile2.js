72349f8b06b903be09385d982f23e5b60052e070
Ranilson de Andrade Saraiva Júnior <ranilsonjunior2@gmail.com>
2022-07-19 14:16:30 -0300

    criado 2 arquivos, css e js.

---
+++ mobile2.js
@@ -0,0 +1,7 @@
+// Esse código está bem resumido e é feito com function.
+function mobile() {
+    let mobileMenu = document.querySelector('.mobile-menu')
+    let navList = document.querySelector('.nav-list')
+    mobileMenu.classList.toggle('ativo')
+    navList.classList.toggle('ativo')
+}
